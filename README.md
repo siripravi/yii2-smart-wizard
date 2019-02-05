298626097c155cbd966ed1459b2b88000f0b0745
Dmitry Zhukov <dmitry@zhukovs.ru>
2019-02-05 18:26:49 +0300

    Added extraButtons and Form validation into the Widget

--- README.md
+++ README.md
@@ -13,12 +13,45 @@ echo Step::widget([
     ],
     'items' => [
         1 => [
-            'icon' => 'fa fa-user,
+            'icon' => 'glyphicon glyphicon-user',
             'label' => 'Step - 1',
             'content' => 'Content 1'
         ],
-        2 => [
-            'icon' => 'fa fa-check',
+        2 => [            
+            'label' => 'Step - 2',
+            'content' => '<h2>Content 2 </h2>'
+        ],
+    ],
+]);
+~~~
+
+## Advanced
+~~~ php
+<?php
+use is7\smartwizard\Step;
+
+echo Step::widget([
+    'widgetOptions' => [
+        'theme' => 'default',
+        'showStepURLhash' => false,
+        'autoAdjustHeight' => false,
+    ],
+    'extraButtons' => [
+        'reset', // Reset Wizard
+        'button' => [
+            'icon' => 'glyphicon glyphicon-ok',
+            'label' => 'Click',
+            'class' => 'btn btn-primary',
+            'onClick' => 'function() { alert("Clicked!"); }',
+        ],
+    ],
+    'items' => [
+        1 => [
+            'icon' => 'glyphicon glyphicon-user',
+            'label' => 'Step - 1',
+            'content' => 'Content 1'
+        ],
+        2 => [            
             'label' => 'Step - 2',
             'content' => '<h2>Content 2 </h2>'
         ],
@@ -29,94 +62,32 @@ echo Step::widget([
 ## Form Validate
 ~~~ php
 <?php $form = ActiveForm::begin([
-    'options' => [
-        'role'=>"form",
-        'data-toggle'=>"validator",
-        'accept-charset'=>"utf-8",
-        'novalidate'=>"true"
-    ]
+    'enableClientValidation' => true,
 ]); 
 
-$wizardId = 'person-wizard';
-
-$jsPersonWizard['btn'] = <<< JS
-// Toolbar extra buttons
-var btnFinish = $('<button></button>').html('<i class="fa fa-flag-checkered"></i> Finish')
-    .addClass('btn btn-primary')
-    .on('click', function(){
-        $.each($('.step-content'), function(){
-            if($(this).find('.has-error').length) {
-                $('a[href="#'+$(this).prop('id')+'"]').addClass('error');
-            }
-        });
-    });
-    
-var btnCancel = $('<button></button>').html('<i class="fa fa-times"></i> Cancel')
-    .addClass('btn btn-default')
-    .on('click', function(){ $('#{$wizardId}').smartWizard('reset'); });
-JS;
-
-$jsPersonWizard['event'] = <<< JS
-$('#{$wizardId}').on('showStep', function(e, anchorObject, stepNumber, stepDirection, stepPosition) {
-    //alert('You are on step '+stepNumber+' now');
-    $('#{$form->id}').yiiActiveForm("resetForm");
-    if(stepPosition === 'first'){
-        $('#prev-btn').addClass('disabled');
-    }else if(stepPosition === 'final'){
-        $('#next-btn').addClass('disabled');
-    }else{
-        $('#prev-btn').removeClass('disabled');
-        $('#next-btn').removeClass('disabled');
-    }
-});
-
-$('#{$wizardId}').on('leaveStep', function(e, anchorObject, stepNumber, stepDirection) {
-    var elmForm = $('#{$wizardId}-form-step-' + stepNumber);
-    if(stepDirection === 'forward' && elmForm){
-        var inputs = elmForm.find('*[id]:visible');
-        data = $('#{$form->id}').data("yiiActiveForm");
-        $.each(data.attributes, function(i, item) {
-            this.status = 3;
-        });
-        $('#{$form->id}').yiiActiveForm("validate");
-        if (elmForm.find(".has-error").length) {
-            return false;
-        }
-
-    }
-    return true;
-
-});
-JS;
-
-$this->registerJs(implode("\n", $jsPersonWizard));
-
-
 echo Step::widget([
-    'id' => $wizardId,
-    'isFormStep' => true,
+    'formId' => $form->id,
+    'extraButtons' => [
+        'submit',
+        'reset',
+    ],
     'widgetOptions' => [
         'theme' => 'default',
         'showStepURLhash' => false,
-        'autoAdjustHeight' => false,
-        'toolbarSettings' => [
-            'toolbarPosition' => 'both',
-            'toolbarExtraButtons' => new JsExpression("[btnFinish, btnCancel]"),
-        ],
+        'autoAdjustHeight' => false,        
     ],
     'items' => [
         1 => [
-            'icon' => 'fa fa-info',
+            'icon' => 'glyphicon glyphicon-info-sign',
             'label' => 'Step - 1 <br /><small>Information</small>',
             'content' => $this->render('_step-1', ['models' => $models, 'form' => $form])
         ],
         2 => [
-            'icon' => 'fa fa-camera',
+            'icon' => 'glyphicon glyphicon-picture',
             'label' => 'Step - 2 <br /><small>Photo</small>',
             'content' => $this->render('_step-2', ['models' => $models, 'form' => $form])
         ],
-        3 => [
-            'icon' => 'fa fa-address-card',
+        3 => [            
             'label' => 'Step - 3 <br /><small>Address</small>',
             'content' => $this->render('_step-3', ['models' => $models, 'form' => $form])
         ],
--- Step.php
+++ Step.php
@@ -1,7 +1,9 @@
 <?php
 namespace is7\smartwizard;
 
+use yii\helpers\BaseInflector;
 use yii\helpers\Html;
+use yii\web\JsExpression;
 
 /**
  * \is7\smartwizard\Step.
@@ -12,20 +14,54 @@ use yii\helpers\Html;
  */
 class Step extends \yii\base\Widget
 {
-    public $toolbarExtraButtons = [];
-    
+
     public $widgetOptions = [];
     
     public $items = [];
     
-    public $isFormStep = false;
-    
+    /**
+     * @var array widget extra buttons
+     * [
+     *   'submit' => [
+     *     'label' => 'Finish',
+     *     'icon' => 'glyphicon glyphicon-ok',
+     *     'class' => 'btn btn-primary',
+     *     'onClick' => 'function() {alert('Click');}',
+     *   ]
+     */
+    public $extraButtons = [];
+
+    /**
+     * @var array widget events
+     * [
+     *   'showStep'  => 'function(e, anchorObject, stepNumber, stepDirection, stepPosition) { console.log('You are on step '+stepNumber+' now'); }',
+     *   'leaveStep' => 'function(e, anchorObject, stepNumber, stepDirection) { console.log('Direction: '+stepDirection); }',
+     * ]
+     */
+    public $events = [];
+
+    /**
+     * @var string Yii Form Id
+     */
+    public $formId = null;
+
+    /**
+     * @var array button names array
+     */
+    private $toolbarExtraButtons = [];
+
     /**
      * Renders the widget.
      */
     public function run()
     {
         $asset = SmartWizardAsset::register($this->getView());
+
+        if ($this->formId && empty($this->extraButtons)) {
+            $this->extraButtons[] = 'submit';
+        }
+        $this->registerExtraButtons();
+        $this->registerEvents();
         $this->registerWidget($asset);
         return $this->renderWidget($asset);
     }
@@ -52,7 +88,7 @@ class Step extends \yii\base\Widget
         $contentArr = [];
         $formStepNum = 0;
         foreach ($this->items as $key => $item) {
-            if($this->isFormStep) {
+            if($this->formId) {
                 $item['content'] = Html::tag('div', $item['content'], ['id' => "{$this->id}-form-step-{$formStepNum}"]);
                 $formStepNum++;
             }
@@ -65,14 +101,95 @@ class Step extends \yii\base\Widget
     public function registerWidget($asset)
     {
         $view = $this->getView();
+        if (!empty($this->toolbarExtraButtons)) {
+            if (isset($this->widgetOptions['toolbarSettings']['toolbarExtraButtons'])) {
+                if (is_string($this->widgetOptions['toolbarSettings']['toolbarExtraButtons'])) {
+                    $this->widgetOptions['toolbarSettings']['toolbarExtraButtons'] = new JsExpression($this->widgetOptions['toolbarSettings']['toolbarExtraButtons']);
+                }
+                $this->widgetOptions['toolbarSettings']['toolbarExtraButtons']->expression .= ".concat([" . implode(', ', $this->toolbarExtraButtons) . "])";
+            }
+            else {
+                $this->widgetOptions['toolbarSettings']['toolbarExtraButtons'] = new JsExpression("[" . implode(', ', $this->toolbarExtraButtons) . "]");
+            }
+        }
         $jsonOptions = \yii\helpers\Json::encode($this->widgetOptions);
-        $js[] = <<< JS
-$('#{$this->id}').smartWizard($jsonOptions);
-JS;
-        
-        $view->registerJs(implode("\n", $js));
+        $view->registerJs("$('#{$this->id}').smartWizard($jsonOptions);");
     }
-    
+
+    public function registerExtraButtons() {
+        foreach ($this->extraButtons as $buttonName => $buttonOptions) {
+            if (is_int($buttonName)) { // array index
+                $buttonName = $buttonOptions;
+                $buttonOptions = [];
+            }
+
+            $buttonOptions['name'] = 'btn'.BaseInflector::camelize($buttonName);
+            $buttonOptions['id']   = BaseInflector::camel2id($buttonOptions['name']);
+            if (!isset($buttonOptions['label'])) {
+                $buttonOptions['label'] = BaseInflector::camel2words($buttonName);
+            }
+            if (!isset($buttonOptions['class'])) {
+                $buttonOptions['class'] = 'btn btn-default';
+            }
+
+            switch ($buttonName) {
+                case 'submit':
+                    if ($this->formId) {
+                        $this->addEvent('showStep',$this->defaultSubmitDisableFunction($buttonOptions['id']));
+                        $this->addEvent('leaveStep',$this->defaultSubmitValidateFunction());
+                        if (!isset($buttonOptions['onClick'])) {
+                            $buttonOptions['onClick'] = $this->defaultSubmitOnClickFunction();
+                        }
+                    }
+                    break;
+                case 'reset':
+                    if (!isset($buttonOptions['onClick'])) {
+                        $buttonOptions['onClick'] = "function(){ $('#{$this->id}').smartWizard('reset'); }";
+                    }
+                    break;
+            }
+
+            $this->registerExtraButton($buttonOptions);
+        }
+    }
+
+    public function registerExtraButton($options) {
+        $view = $this->getView();
+
+        if (isset($options['icon'])) {
+            $options['label'] = Html::tag('i',null,['class' => $options['icon']]).' '.$options['label'];
+        }
+
+        $jQuery   = ["$('<button></button>')"];
+        $jQuery[] = "html('{$options['label']}')";
+        $jQuery[] = "prop('id','{$options['id']}')";
+        $jQuery[] = "addClass('{$options['class']}')";
+        if (isset($options['onClick'])) {
+            $jQuery[] = "on('click', {$options['onClick']})";
+        }
+        $view->registerJs("var {$options['name']} = ".implode('.', $jQuery).";");
+        $this->toolbarExtraButtons[] = $options['name'];
+    }
+
+    public function registerEvents() {
+        $view = $this->getView();
+        foreach ($this->events as $event => $functions) {
+            if (!is_array($functions)) {
+                $functions = [ $functions ];
+            }
+            foreach ($functions as $function) {
+                $view->registerJs("$('#{$this->id}').on('{$event}', {$function});");
+            }
+        }
+    }
+
+    public function addEvent($event, $function) {
+        if (isset($this->events[$event]) && !is_array([$this->events[$event]])) {
+            $this->events[$event] = [$this->events[$event]];
+        }
+        $this->events[$event][] = $function;
+    }
+
     public function getTheme($asset)
     {
         $view = $this->getView();
@@ -94,5 +211,52 @@ JS;
         $view->registerCssFile($asset->baseUrl.'/css/'.$cssFile, ['depends' => ['\is7\smartwizard\SmartWizardAsset']]);
         
         return $theme;
+    }
+
+    private function defaultSubmitOnClickFunction() {
+        return <<<JS
+function() {
+    $.each($('.step-content'), function() {
+        if ($(this).find('.has-error').length) {
+            $('a[href="#'+$(this).prop('id')+'"]').addClass('error');
+        }
+    });
+}
+JS;
+    }
+
+    private function defaultSubmitDisableFunction($buttonId) {
+        return <<<JS
+function(e, anchorObject, stepNumber, stepDirection, stepPosition) {
+    $('#{$this->formId}').yiiActiveForm("resetForm");
+    if(stepPosition === 'first') {
+        $('#{$buttonId}').attr('disabled', 'disabled');
+    } else if(stepPosition === 'final'){
+        $('#{$buttonId}').removeAttr('disabled');
+    } else {
+        $('#{$buttonId}').attr('disabled', 'disabled');
+    }
+}
+JS;
+    }
+
+    private function defaultSubmitValidateFunction() {
+        return <<<JS
+function(e, anchorObject, stepNumber, stepDirection) {
+    var elmForm = $('#{$this->id}-form-step-' + stepNumber);
+    if(stepDirection === 'forward' && elmForm){
+        var inputs = elmForm.find('*[id]:visible');
+        data = $('#{$this->formId}').data("yiiActiveForm");
+        $.each(data.attributes, function(i, item) {
+            this.status = 3;
+        });
+        $('#{$this->formId}').yiiActiveForm("validate");
+        if (elmForm.find(".has-error").length) {
+            return false;
+        }
+    }
+    return true;
+}
+JS;
     }
 }
